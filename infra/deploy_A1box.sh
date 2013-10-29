c8359907ff54ce5eeda8fc47cb83fac1c875cb6e
TimurNurlygayanov <tnurlygayanov@mirantis.com>
2013-10-29 12:00:38 +0400

    Added additional script for lab deployment.
    Change-Id: I79c160d22f32b21b38bf7c585778c1dac41df2ed

---
+++ infra/deploy_A1box.sh
@@ -0,0 +1,47 @@
+set timeout 1200
+
+send_user "\n\nStart to login to the test bed...\n\n"
+
+spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
+expect "password"
+send -- [lindex $argv 2]
+send -- "\n"
+expect "*root@A1box*"
+
+send -- "yum makecache -y\n"
+expect "*root@A1box*"
+send -- "yum update -y\n"
+expect "*root@A1box*"
+send -- "yum install python-memcached -y\n"
+expect "*root@A1box*"
+
+send -- "sed -i \"s/LAB_HOST=''/LAB_HOST='"
+send -- [lindex $argv 3]
+send -- "'/\" /etc/murano-deployment/lab-binding.rc\n"
+expect "*root@A1box*"
+send -- "sed -i \"s/BRANCH_NAME=''/BRANCH_NAME='"
+send -- [lindex $argv 4]
+send -- "'/\" /etc/murano-deployment/lab-binding.rc\n"
+expect "*root@A1box*"
+send -- "rm -rf /opt/git\n"
+expect "*root@A1box*"
+send -- "mkdir -p /opt/git\n"
+expect "*root@A1box*"
+send -- "cd /opt/git\n"
+expect "*root@A1box*"
+send -- "git clone https://github.com/stackforge/murano-deployment -b "
+send -- [lindex $argv 4]
+send -- "\n"
+expect "*root@A1box*"
+
+send -- "cd murano-deployment/devbox-scripts/\n"
+expect "*root@A1box*"
+send -- "./murano-git-install.sh prerequisites\n"
+expect "*root@A1box*"
+send -- "./murano-git-install.sh install\n"
+expect "*root@A1box*"
+
+send -- "sed -i \"s/connection = sqlite:///murano.sqlite/connection = mysql://murano:swordfish@localhost:3306/murano/\" /etc/murano-api/murano-api.conf\n"
+expect "*root@A1box*"
+send -- "/etc/init.d/murano-api restart\n"
+expect "*root@A1box*"
