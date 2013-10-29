adb310e838248ba4777038bf9c5c3e2f98e76e6a
TimurNurlygayanov <tnurlygayanov@mirantis.com>
2013-10-29 18:29:04 +0400

    Fixed deployment script for dashboard
    Change-Id: Ib0d38bbcc35e327569fa626c48e1697ceaf459e6

--- infra/deploy_dashboard.sh
+++ infra/deploy_dashboard.sh
@@ -6,53 +6,15 @@ spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
 expect "password"
 send -- [lindex $argv 2]
 send -- "\n"
-expect "*root@A2box*"
-
-send -- "yum makecache -y\n"
-expect "*root@A2box*"
-send -- "yum update -y\n"
-expect "*root@A2box*"
-send -- "yum install python-memcached -y\n"
-expect "*root@A2box*"
-
-send -- "sed -i \"s/LAB_HOST=''/LAB_HOST='"
-send -- [lindex $argv 4]
-send -- "'/\" /etc/murano-deployment/lab-binding.rc\n"
-expect "*root@A2box*"
-send -- "sed -i \"s/BRANCH_NAME=''/BRANCH_NAME='"
-send -- [lindex $argv 5]
-send -- "'/\" /etc/murano-deployment/lab-binding.rc\n"
-expect "*root@A2box*"
-send -- "rm -rf /opt/git\n"
-expect "*root@A2box*"
-send -- "mkdir -p /opt/git\n"
-expect "*root@A2box*"
-send -- "cd /opt/git\n"
-expect "*root@A2box*"
-send -- "git clone https://github.com/stackforge/murano-deployment -b "
-send -- [lindex $argv 5]
-send -- "\n"
-expect "*root@A2box*"
-
-send -- "cd murano-deployment/devbox-scripts/\n"
-expect "*root@A2box*"
-send -- "./murano-git-install.sh prerequisites\n"
-expect "*root@A2box*"
-send -- "./murano-git-install.sh install\n"
-expect "*root@A2box*"
-
-send -- "sed -i \"s/connection = sqlite:///murano.sqlite/connection = mysql://murano:swordfish@localhost:3306/murano/\" /etc/murano-api/murano-api.conf\n"
-expect "*root@A2box*"
-send -- "/etc/init.d/murano-api restart\n"
-expect "*root@A2box*"
+expect "*root@??box*"
 
 send -- "cd /opt/git/murano-dashboard\n"
-expect "*root@A2box*"
+expect "*root@??box*"
 send -- "git fetch https://review.openstack.org/stackforge/murano-dashboard "
 send -- [lindex $argv 3] 
 send -- " && git checkout FETCH_HEAD\n"
-expect "*root@A2box*"
+expect "*root@??box*"
 send -- "sh setup-centos.sh install\n"
-expect "*root@A2box*"
+expect "*root@??box*"
 
 send -- "exit\n"
