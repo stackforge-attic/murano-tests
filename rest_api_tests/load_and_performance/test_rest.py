5b32b19a3cc8e1197348bdef9a91dce3c58f7f42
Sergey Murashov <smurashov@mirantis.com>
2013-10-31 15:17:07 +0400

    Add changes in stress tests
    Change-Id: I86af3ff448eb76fbd843928d83e6ae5d6291ee48

--- rest_api_tests/load_and_performance/test_rest.py
+++ rest_api_tests/load_and_performance/test_rest.py
@@ -14,31 +14,37 @@ LOG = logging.getLogger(' REST service tests')
 
 class TestSuite(FunkLoadTestCase):
 
-    clear = False
-
     def setUp(self):
-        self.url = self.conf_get('main', 'url')
-        self.action_set_headers()
-
-    def action_set_headers(self):
         self.clearHeaders()
+        self.url = self.conf_get('main', 'url')
         config = ConfigParser.RawConfigParser()
         config.read('config.ini')
         user = config.get('keystone', 'user')
         password = config.get('keystone', 'password')
+        tenant = config.get('keystone', 'tenant')
         keystone_url = config.get('keystone', 'url')
-
-        keystone_client = ksclient.Client(username=user,
-                                          password=password,
-                                          tenant_name=user,
-                                          auth_url=keystone_url)
-        token = keystone_client.auth_token
-
+        keystone_client = ksclient.Client(username=user, password=password,
+                                    tenant_name=tenant, auth_url=keystone_url)
+        token = str(keystone_client.auth_token)
+        self.environments = []
         self.setHeader('X-Auth-Token', token)
+
+    def tearDown(self):
+        for i in self.environments:
+            try:
+                self.action_delete_environment(i)
+            except:
+                pass
+
+    def generate_num(self): 
+        p=""
+        for i in xrange(10):
+            p += str(random.randint(0,10))
+        return p
 
     def action_create_environment(self):
         self.setHeader('Content-Type', 'application/json')
-        name = "Environment" + str(random.randint(1, 10000))
+        name = "Environment" + self.generate_num()
         body = '{"name": "%s"}' % name
 
         response = self.post(self.url, params=Data('application/json', body),
@@ -49,13 +55,11 @@ class TestSuite(FunkLoadTestCase):
         return str(result['id'])
 
     def action_delete_environment(self, env_id):
-        self.action_set_headers()
         url = self.url + '/' + str(env_id)
         response = self.delete(url, description="Delete Environment")
         assert response.code == 200
 
     def action_get_session_for_environment(self, env_id):
-        self.action_set_headers()
         self.setHeader('Content-Type', 'application/json')
 
         url = self.url + '/' + str(env_id) + '/configure'
@@ -67,39 +71,142 @@ class TestSuite(FunkLoadTestCase):
 
 
     def action_create_service_ad(self, env_id, session_id, name='ad'):
-        self.action_set_headers()
         self.setHeader('Content-Type', 'application/json')
         self.setHeader('X-Configuration-Session', str(session_id))
 
-        body = ('{"name": "%s", "configuration": "standalone",'
-                '"adminPassword": "P@ssw0rd", "domain": "test_ad",'
-                '"units": [{"id": "d08887df15b94178b244904b506fe85b",'
-                '"isMaster": true,"location": "west-dc"}]}') % name
+        body = ('{"type": "activeDirectory", "name": "%s",'
+                '"adminPassword": "P@ssw0rd", "domain": "%s.local",'
+                '"availabilityZone": "nova", "unitNamingPattern": "",'
+                '"flavor": "m1.medium", "osImage": {"type": "ws-2012-std",'
+                '"name": "ws-2012-std",'
+                '"title": "Windows Server 2012 Standard"},'
+                '"configuration": "standalone",'
+                '"units": [{"isMaster": "True", "recoveryPassword": "P@ssw0rd",'
+                '"location": "west-dc"}]}') % (name,name)
 
-        url = self.url + '/' + env_id + '/activeDirectories'
+        url = self.url + '/' + env_id + '/services'
 
         response = self.post(url, params=Data('application/json', body),
                              description="Create AD service")
         assert response.code == 200
 
     def action_create_service_iis(self, env_id, session_id, name='iis'):
-        self.action_set_headers()
         self.setHeader('Content-Type', 'application/json')
         self.setHeader('X-Configuration-Session', str(session_id))
 
-        body = ('{"name": "%s","domain": {"name": "ad_test",'
-                '"credentials": {"username":"admin", "password":"123"}'
-                '}, "credentials": {"username":"admin", "password":"12."},'
-                '"location": "temp","units":[{"id":"4334"}]}') % name
+        body = ('{"type": "webServer", "domain": "",'
+                '"availabilityZone": "nova", "name": "%s",'
+                '"adminPassword": "P@ssw0rd", "unitNamingPattern": "",'
+                '"osImage": {"type": "ws-2012-std", "name": "ws-2012-std",'
+                '"title": "Windows Server 2012 Standard"},'
+                '"units": [{}], "credentials": {"username": "Administrator",'
+                '"password": "P@ssw0rd"},'
+                '"flavor": "m1.medium"}') % name
 
-        url = self.url + '/' + env_id + '/webServers'
+        url = self.url + '/' + env_id + '/services'
         response = self.post(url, params=Data('application/json', body),
                              description="Create IIS service")
         assert response.code == 200
 
+    def action_create_service_aspnet(self, env_id, session_id, name='aspnet'):
+        self.setHeader('Content-Type', 'application/json')
+        self.setHeader('X-Configuration-Session', str(session_id))
+
+        body = ('{"type": "aspNetApp", "domain": "",'
+                '"availabilityZone": "nova", "name": "%s", "repository":'
+                '"git://github.com/Mirantis/murano-mvc-demo.git",'
+                '"adminPassword": "P@ssw0rd", "unitNamingPattern": "",'
+                '"osImage": {"type": "ws-2012-std", "name": "ws-2012-std",'
+                '"title": "Windows Server 2012 Standard"},'
+                '"units": [{}], "credentials": {"username": "Administrator",'
+                '"password": "P@ssw0rd"},'
+                '"flavor": "m1.medium"}') % name
+
+        url = self.url + '/' + env_id + '/services'
+        response = self.post(url, params=Data('application/json', body),
+                             description="Create ASP.Net service")
+        assert response.code == 200
+
+    def action_create_service_iis_farm(self, env_id, session_id, name='iis_farm'):
+        self.setHeader('Content-Type', 'application/json')
+        self.setHeader('X-Configuration-Session', str(session_id))
+
+        body = ('{"type": "webServerFarm", "domain": "",'
+                '"availabilityZone": "nova", "name": "%s",'
+                '"adminPassword": "P@ssw0rd", "loadBalancerPort": 80,'
+                '"unitNamingPattern": "",'
+                '"osImage": {"type": "ws-2012-std", "name": "ws-2012-std",'
+                '"title": "Windows Server 2012 Standard"},'
+                '"units": [{}, {}],'
+                '"credentials": {"username": "Administrator",'
+                '"password": "P@ssw0rd"}, "flavor": "m1.medium"}') % name
+
+        url = self.url + '/' + env_id + '/services'
+        response = self.post(url, params=Data('application/json', body),
+                             description="Create IIS farm service")
+        assert response.code == 200
+
+    def action_create_service_aspnet_farm(self, env_id, session_id,
+                                          name='aspnet_farm'):
+        self.setHeader('Content-Type', 'application/json')
+        self.setHeader('X-Configuration-Session', str(session_id))
+
+        body = ('{"type": "aspNetAppFarm", "domain": "",'
+                '"availabilityZone": "nova", "name": "%s",'
+               '"repository": "git://github.com/Mirantis/murano-mvc-demo.git",'
+                '"adminPassword": "P@ssw0rd", "loadBalancerPort": 80,'
+                '"unitNamingPattern": "",'
+                '"osImage": {"type": "ws-2012-std", "name": "ws-2012-std",'
+                '"title": "Windows Server 2012 Standard"},'
+                '"units": [{}, {}],'
+                '"credentials":  {"username": "Administrator",'
+                '"password": "P@ssw0rd"}, "flavor": "m1.medium"}') % name
+
+        url = self.url + '/' + env_id + '/services'
+        response = self.post(url, params=Data('application/json', body),
+                             description="Create ASP.Net farm service")
+        assert response.code == 200
+
+    def action_create_service_sql(self, env_id, session_id, name='sql'):
+        self.setHeader('Content-Type', 'application/json')
+        self.setHeader('X-Configuration-Session', str(session_id))
+
+        body = ('{"type": "msSqlServer", "domain": "",'
+                '"availabilityZone": "nova", "name": "%s",'
+                '"adminPassword": "P@ssw0rd", "unitNamingPattern": "",'
+                '"saPassword": "P@ssw0rd", "mixedModeAuth": "True",'
+                '"osImage": {"type": "ws-2012-std", "name": "ws-2012-std",'
+                '"title": "Windows Server 2012 Standard"},"units": [{}],'
+                '"credentials": {"username": "Administrator",'
+                '"password": "P@ssw0rd"}, "flavor": "m1.medium"}') % name
+
+        url = self.url + '/' + env_id + '/services'
+        response = self.post(url, params=Data('application/json', body),
+                             description="Create MSSQL service")
+        assert response.code == 200
+
+    def action_create_service_linux_agent(self, env_id, session_id,
+                                          name='linuxAgent'):
+        self.setHeader('Content-Type', 'application/json')
+        self.setHeader('X-Configuration-Session', str(session_id))
+
+        body = ('{"availabilityZone": "nova", "name": "%s",'
+                '"deployTelnet": "True", "unitNamingPattern": "",'
+                '"osImage": {"type": "linux",'
+                '"name": "F18-x86_64-cfntools-MURANO",'
+                '"title": "Linux with vNext agent"}, "units": [{}],'
+                '"flavor": "m1.medium", "type": "linuxTelnetService"}') % name
+
+        url = self.url + '/' + env_id + '/services'
+        response = self.post(url, params=Data('application/json', body),
+                             description="Create Linux Agent service")
+        assert response.code == 200
+
     def test_create_and_delete_environment(self):
         env_id = self.action_create_environment()
+        self.environments.append(env_id)
         self.action_delete_environment(env_id)
+        self.environments.pop(self.environments.index(env_id))
 
     def test_get_list_of_environments(self):
         response = self.get(self.url, description="Get List Of Environments")
@@ -107,35 +214,84 @@ class TestSuite(FunkLoadTestCase):
 
     def test_create_environment_with_ad(self):
         env_id = self.action_create_environment()
+        self.environments.append(env_id)
         session_id = self.action_get_session_for_environment(env_id)
         self.action_create_service_ad(env_id, session_id, 'test1')
         self.action_create_service_ad(env_id, session_id, 'test2')
         self.action_create_service_ad(env_id, session_id, 'test3')
         self.action_delete_environment(env_id)
+        self.environments.pop(self.environments.index(env_id))
 
     def test_create_environment_with_iis(self):
         env_id = self.action_create_environment()
+        self.environments.append(env_id)
         session_id = self.action_get_session_for_environment(env_id)
         self.action_create_service_iis(env_id, session_id, 'test1')
         self.action_create_service_iis(env_id, session_id, 'test2')
         self.action_create_service_iis(env_id, session_id, 'test3')
         self.action_delete_environment(env_id)
+        self.environments.pop(self.environments.index(env_id))
+
+    def test_create_environment_with_aspnet(self):
+        env_id = self.action_create_environment()
+        self.environments.append(env_id)
+        session_id = self.action_get_session_for_environment(env_id)
+        self.action_create_service_aspnet(env_id, session_id, 'test1')
+        self.action_delete_environment(env_id)
+        self.environments.pop(self.environments.index(env_id))
+
+    def test_create_environment_with_iis_farm(self):
+        env_id = self.action_create_environment()
+        self.environments.append(env_id)
+        session_id = self.action_get_session_for_environment(env_id)
+        self.action_create_service_iis_farm(env_id, session_id, 'test1')
+        self.action_delete_environment(env_id)
+        self.environments.pop(self.environments.index(env_id))
+
+    def test_create_environment_with_aspnet_farm(self):
+        env_id = self.action_create_environment()
+        self.environments.append(env_id)
+        session_id = self.action_get_session_for_environment(env_id)
+        self.action_create_service_aspnet_farm(env_id, session_id, 'test1')
+        self.action_delete_environment(env_id)
+        self.environments.pop(self.environments.index(env_id))
+
+    def test_create_environment_with_sql(self):
+        env_id = self.action_create_environment()
+        self.environments.append(env_id)
+        session_id = self.action_get_session_for_environment(env_id)
+        self.action_create_service_sql(env_id, session_id, 'test1')
+        self.action_delete_environment(env_id)
+        self.environments.pop(self.environments.index(env_id))
+
+    def test_create_environment_with_linux_agent(self):
+        env_id = self.action_create_environment()
+        self.environments.append(env_id)
+        session_id = self.action_get_session_for_environment(env_id)
+        self.action_create_service_linux_agent(env_id, session_id, 'test1')
+        self.action_delete_environment(env_id)
+        self.environments.pop(self.environments.index(env_id))
 
     def mix_for_load_testing(self):
-        k = random.randint(1, 100)
-
-        if k < 10:
-            # 10%
-            return self.test_get_list_of_environments()
-        elif k < 40:
-            # 30%
+        k = random.randint(0,100)
+        if k < 5:
             return self.test_create_and_delete_environment()
-        elif k < 70:
-            # 30 %
+        elif k < 10:
+            return self.test_get_list_of_environments()
+        elif k < 23:
             return self.test_create_environment_with_ad()
-        else:
-            # 30 %
+        elif k < 36:
             return self.test_create_environment_with_iis()
+        elif k < 49:
+            return self.test_create_environment_with_aspnet()
+        elif k < 62:
+            return self.test_create_environment_with_iis_farm()
+        elif k < 75:
+            return self.test_create_environment_with_aspnet_farm()
+        elif k < 88:
+            return self.test_create_environment_with_sql()
+        elif k < 100:
+            return self.test_create_environment_with_linux_agent()
 
 
 if __name__ == '__main__':
