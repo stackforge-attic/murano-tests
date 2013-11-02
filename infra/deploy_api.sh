0bc70a0487b266cb5725e58da20ab1bd6726e662
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2013-11-03 03:55:37 +0400

    Fixed commit-deployment scripts
    Change-Id: I06e2c902cc292901be4dd46bcbc5908f479bebb5

--- infra/deploy_api.sh
+++ infra/deploy_api.sh
@@ -3,20 +3,17 @@ set timeout 1200
 send_user "\n\nStart to login to the test bed...\n\n"
 
 spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
-expect "password"
-send -- [lindex $argv 2]
-send -- "\n"
-expect "*root@A1box*"
+expect "*@murano-devbox*"
 
 send -- "cd /opt/git/murano-api\n"
-expect "*root@A1box*"
-send -- "sh setup-centos.sh uninstall\n"
-expect "*root@A1box*"
+expect "*@murano-devbox*"
+send -- "sh setup-centos.sh uninstall; sh setup.sh uninstall\n"
+expect "*@murano-devbox*"
 send -- "git fetch https://review.openstack.org/stackforge/murano-api "
-send -- [lindex $argv 3]
+send -- [lindex $argv 2]
 send -- " && git checkout FETCH_HEAD\n"
-expect "*root@A1box*"
-send -- "sh setup-centos.sh install\n"
-expect "*root@A1box*"
+expect "*@murano-devbox*"
+send -- "sh setup-centos.sh install; sh setup.sh install\n"
+expect "*@murano-devbox*"
 
 send -- "exit\n"
--- infra/deploy_dashboard.sh
+++ infra/deploy_dashboard.sh
@@ -3,18 +3,17 @@ set timeout 1200
 send_user "\n\nStart to login to the test bed...\n\n"
 
 spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
-expect "password"
-send -- [lindex $argv 2]
-send -- "\n"
-expect "*root@??box*"
+expect "*@murano-devbox*"
 
 send -- "cd /opt/git/murano-dashboard\n"
-expect "*root@??box*"
+expect "*@murano-devbox*"
 send -- "git fetch https://review.openstack.org/stackforge/murano-dashboard "
-send -- [lindex $argv 3] 
+send -- [lindex $argv 2]
 send -- " && git checkout FETCH_HEAD\n"
-expect "*root@??box*"
+expect "*@murano-devbox*"
 send -- "sh setup-centos.sh install\n"
-expect "*root@??box*"
+expect "*@murano-devbox*"
+send -- "sh setup.sh install\n"
+expect "*@murano-devbox*"
 
 send -- "exit\n"
