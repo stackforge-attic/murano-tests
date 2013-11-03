ce5e3fdad2f0129ddbbc403f73ada401ee50dd1c
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2013-11-03 04:07:15 +0400

    Fixed users for deployment scripts
    Change-Id: I549a3d93ab85b87da34996d2d66a8469913d8d01

--- infra/deploy_api.sh
+++ infra/deploy_api.sh
@@ -5,6 +5,9 @@ send_user "\n\nStart to login to the test bed...\n\n"
 spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
 expect "*@murano-devbox*"
 
+send -- "sudo su\n"
+expect "*@murano-devbox*"
+
 send -- "cd /opt/git/murano-api\n"
 expect "*@murano-devbox*"
 send -- "sh setup-centos.sh uninstall; sh setup.sh uninstall\n"
--- infra/deploy_dashboard.sh
+++ infra/deploy_dashboard.sh
@@ -5,6 +5,9 @@ send_user "\n\nStart to login to the test bed...\n\n"
 spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
 expect "*@murano-devbox*"
 
+send -- "sudo su\n"
+expect "*@murano-devbox*"
+
 send -- "cd /opt/git/murano-dashboard\n"
 expect "*@murano-devbox*"
 send -- "git fetch https://review.openstack.org/stackforge/murano-dashboard "
