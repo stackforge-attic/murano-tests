895d45da08f6c93f8d12db6952b2f5ea4627d1f2
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2013-11-05 10:02:43 +0400

    Refactoring on infra scripts
    Change-Id: I764bc3b6ef1cbb782ec1e7b18c02f61f28dd7792

--- infra/deploy_component.sh
+++ infra/deploy_component.sh
@@ -1,3 +1,8 @@
+###
+### Use:
+###   expect deploy_component.sh user 10.10.10.10 /refs/for/master/344332 murano-api
+###
+
 set timeout 1200
 
 send_user "\n\nStart to login to the test bed...\n\n"
@@ -8,11 +13,15 @@ expect "*@murano-devbox*"
 send -- "sudo su\n"
 expect "*@murano-devbox*"
 
-send -- "cd /opt/git/murano-api\n"
+send -- "cd /opt/git/"
+send -- [lindex $argv 3]
+send -- "\n"
 expect "*@murano-devbox*"
 send -- "sh setup-centos.sh uninstall; sh setup.sh uninstall\n"
 expect "*@murano-devbox*"
-send -- "git fetch https://review.openstack.org/stackforge/murano-api "
+send -- "git fetch https://review.openstack.org/stackforge/"
+send -- [lindex $argv 3]
+send -- " "
 send -- [lindex $argv 2]
 send -- " && git checkout FETCH_HEAD\n"
 expect "*@murano-devbox*"
