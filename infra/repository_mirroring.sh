553781311463460a8004139b7eacb6cbdbea7c86
TimurNurlygayanov <tnurlygayanov@mirantis.com>
2013-11-05 19:23:18 +0400

    Added passwod validation for sync script
    Change-Id: I995a2eb5732637f81cc655a80a8a76c71593093e

--- infra/repository_mirroring.sh
+++ infra/repository_mirroring.sh
@@ -8,15 +8,17 @@ set timeout 1200
 send_user "\n\nStart to login to the test bed...\n\n"
 
 spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
+expect "*password*"
+send -- [lindex $argv 3]
 expect "*@reposync*"
 
 send -- "git clone --mirror https://github.com/stackforge/"
-send -- [lindex $argv 4]
+send -- [lindex $argv 5]
 send -- ".git\n"
 expect "*@reposync*"
 
 send -- "cd "
-send -- [lindex $argv 4]
+send -- [lindex $argv 5]
 send -- ".git\n"
 expect "*@reposync*"
 
@@ -24,9 +26,9 @@ send -- "git fetch origin +refs/meta/*:refs/meta/*"
 expect "*@reposync*"
 
 send -- "git push --mirror ssh://"
-send -- [lindex $argv 3]
-send -- "@gerrit.mirantis.com:29418/murano/"
 send -- [lindex $argv 4]
+send -- "@gerrit.mirantis.com:29418/murano/"
+send -- [lindex $argv 5]
 send -- ".git\n"
 expect "*@reposync*"
 
