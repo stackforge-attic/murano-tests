f266e993e5373b3a00438bc7871f69ef1006cbf2
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2013-11-06 21:03:57 +0400

    Fixed script for reports merge
    Change-Id: Ie5eb622c916dc1b13658d6089f5609ce3a0ecd9d

--- infra/merge_robot_results.py
+++ infra/merge_robot_results.py
@@ -12,36 +12,44 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-###
-### Please use this script for the XML RobotFramework reports merging:
-###     python merge_robot_results.py output1.xml output2.xml
-###
+msg = """
+      ### Please use this script for the
+      ### XML RobotFramework reports merging:
+      ###   python merge_robot_results.py output1.xml output2.xml
+      """
 
+### To install requirements:
+###    sudo pip install BeautifulSoup4 python-dateutil
 import sys
 from dateutil import parser
 from bs4 import BeautifulSoup
 
 files = []
-for i in range(len(sys.argv)-1):
-    print sys.argv[i+1]
-    f = open(sys.argv[i+1], 'r').read()
-    res = BeautifulSoup(f)
-    files.append(res)
+for i in range(len(sys.argv) - 1):
+    try:
+        f = open(sys.argv[i + 1], 'r').read()
+        res = BeautifulSoup(f)
+        files.append(res)
+    except:
+        print "Incorrect XML file:", sys.argv[i + 1]
 
-for test in files[0].robot.suite.find_all('test'):
-    for res in files[1:]:
-        for retest in res.robot.suite.find_all('test'):
-            name = test['name']
-            status = test.status['status']
-            end_time = parser.parse(test.status['endtime'])
-            retest_end_time = parser.parse(retest.status['endtime'])
-            if (test['name'] == retest['name'] and
-                status != retest.status['status'] and
-                end_time > retest_end_time):
-                test.status['status'] = retest.status['status']
-                test.status['starttime'] = retest.status['starttime']
-                test.status['endtime'] = retest.status['endtime']
+if files:
+    for test in files[0].robot.suite.find_all('test'):
+        for f in files[1:]:
+            for retest in f.robot.suite.find_all('test'):
+                name = test['name']
+                status = test.status['status']
+                end_time = parser.parse(test.status['endtime'])
+                retest_end_time = parser.parse(retest.status['endtime'])
+                if (test['name'] == retest['name'] and
+                    status != retest.status['status'] and
+                    end_time < retest_end_time):
+                    test.status['status'] = retest.status['status']
+                    test.status['starttime'] = retest.status['starttime']
+                    test.status['endtime'] = retest.status['endtime']
 
-result = open('result.xml', 'w')
-result.write(files[0].prettify())
-result.close()
+    result = open('result.xml', 'w')
+    result.write(files[0].prettify())
+    result.close()
+else:
+    print msg
