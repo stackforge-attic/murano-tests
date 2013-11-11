b9d46b2d3bcbc9477c941bb1493c3faf48ccd131
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2013-11-11 20:46:13 +0400

    Fixed expect scripts
    Change-Id: Ib843de0a5c1b9eaa1d7105f90509bee241be5723

--- infra/deploy_component.sh
+++ infra/deploy_component.sh
@@ -11,25 +11,25 @@ spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
 expect "*@murano-devbox*"
 
 send -- "sudo su\n"
-expect "*@murano-devbox*"
+expect "*#*"
 
 send -- "cd /opt/git/"
 send -- [lindex $argv 3]
 send -- "\n"
-expect "*@murano-devbox*"
+expect "*#*"
 send -- "sh setup-centos.sh uninstall; sh setup.sh uninstall\n"
-expect "*@murano-devbox*"
+expect "*#*"
 send -- "git fetch https://review.openstack.org/stackforge/"
 send -- [lindex $argv 3]
 send -- " "
 send -- [lindex $argv 2]
 send -- " && git checkout FETCH_HEAD\n"
-expect "*@murano-devbox*"
+expect "*#*"
 send -- "sh setup-centos.sh install; sh setup.sh install\n"
-expect "*@murano-devbox*"
+expect "*#*"
 send -- "service "
 send -- [lindex $argv 3]
 send -- " restart\n"
-expect "*@murano-devbox*"
+expect "*#*"
 
 send -- "exit\n"
