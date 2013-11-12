e07cd2231cc8f04c629fc6650ecfe18a6198410f
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2013-11-12 10:37:16 +0400

    Fixed expect scripts
    Change-Id: If3a6f315d70db119e0fcb7e2997c9947325e33a9

--- infra/deploy_component.sh
+++ infra/deploy_component.sh
@@ -11,25 +11,29 @@ spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
 expect "@murano"
 
 send -- "sudo su\n"
-expect "@murano"
+expect "#"
 
 send -- "cd /opt/git/"
 send -- [lindex $argv 3]
 send -- "\n"
-expect "@murano"
-send -- "sh setup-centos.sh uninstall; sh setup.sh uninstall\n"
-expect "@murano"
+expect "#"
+send -- "sh setup-centos.sh uninstall > 1.log\n"
+expect "#"
+send -- "sh setup.sh uninstall > 2.log\n"
+expect "#"
 send -- "git fetch https://review.openstack.org/stackforge/"
 send -- [lindex $argv 3]
 send -- " "
 send -- [lindex $argv 2]
 send -- " && git checkout FETCH_HEAD\n"
-expect "@murano"
-send -- "sh setup-centos.sh install; sh setup.sh install\n"
-expect "@murano"
+expect "#"
+send -- "sh setup-centos.sh install > 3.log\n"
+except "#"
+send -- "sh setup.sh install > 4.log\n"
+expect "#"
 send -- "service "
 send -- [lindex $argv 3]
 send -- " restart\n"
-expect "@murano"
+expect "#"
 
 send -- "exit\n"
--- infra/deploy_vm.sh
+++ infra/deploy_vm.sh
@@ -10,72 +10,76 @@ spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
 expect "@murano-devbox"
 
 send -- "sudo su\n"
-expect "@murano-devbox"
+expect "#"
 
 send -- "sed -i \"s/LAB_HOST=''/LAB_HOST='"
 send -- [lindex $argv 2]
 send -- "'/\" /etc/murano-deployment/lab-binding.rc\n"
-expect "@murano-devbox"
+expect "#"
 
 send -- "sed -i \"s/RABBITMQ_VHOST='A001box'/RABBITMQ_VHOST='"
 send -- [lindex $argv 6]
 send -- "'/\" /etc/murano-deployment/lab-binding.rc\n"
-expect "@murano-devbox"
+expect "#"
 
 send -- "sed -i \"s/RABBITMQ_LOGIN='A001box'/RABBITMQ_LOGIN='"
 send -- [lindex $argv 6]
 send -- "'/\" /etc/murano-deployment/lab-binding.rc\n"
-expect "@murano-devbox"
+expect "#"
 
 send -- "sed -i \"s/BRANCH_NAME=''/BRANCH_NAME='"
 send -- [lindex $argv 3]
 send -- "'/\" /etc/murano-deployment/lab-binding.rc\n"
-expect "@murano-devbox"
-send -- "rm -rf /opt/git; mkdir -p /opt/git; cd /opt/git\n"
-expect "@murano-devbox"
+expect "#"
+send -- "rm -rf /opt/git\n"
+expect "#"
+send -- "mkdir -p /opt/git\n"
+expect "#"
+send -- "cd /opt/git\n"
+expect "#"
 send -- "git clone https://github.com/stackforge/murano-deployment -b "
 send -- [lindex $argv 3]
 send -- "\n"
-expect "@murano-devbox"
+expect "#"
 
 set timeout 600
 send -- "cd murano-deployment/devbox-scripts/\n"
-expect "@murano-devbox"
-send -- "./murano-git-install.sh prerequisites\n"
-expect "@murano-devbox"
-send -- "./murano-git-install.sh install\n"
-expect "@murano-devbox"
+expect "#"
+send -- "./murano-git-install.sh prerequisites > 1.log\n"
+expect "#"
+send -- "./murano-git-install.sh install > 2.log\n"
+expect "#"
 
 set timeout 30
 send -- "sed -i \"s/connection = sqlite:\\/\\/\\/murano.sqlite/connection = mysql:\\/\\/murano:swordfish@localhost:3306\\/murano/\" /etc/murano-api/murano-api.conf\n"
-expect "@murano-devbox"
+expect "#"
 
 send -- "sed -i \"s/port = 5672/port = "
 send -- [lindex $argv 4]
 send -- "/\" /etc/murano-api/murano-api.conf\n"
-expect "@murano-devbox"
+expect "#"
 send -- "sed -i \"s/ssl = False/ssl = "
 send -- [lindex $argv 5]
 send -- "/\" /etc/murano-api/murano-api.conf\n"
-expect "@murano-devbox"
+expect "#"
 
 send -- "sed -i \"s/port = 5672/port = "
 send -- [lindex $argv 4]
 send -- "/\" /etc/murano-conductor/conductor.conf\n"
-expect "@murano-devbox"
+expect "#"
 send -- "sed -i \"s/ssl = False/"
 send -- [lindex $argv 5]
 send -- "/\" /etc/murano-conductor/conductor.conf\n"
-expect "@murano-devbox"
+expect "#"
 
 send -- "sed -i \"s/\\\"BootFromVolume\\\": true,//\" /etc/murano-conductor/data/templates/cf/Linux.template\n"
-expect "@murano-devbox"
+expect "#"
 send -- "sed -i \"s/\\\"BootFromVolume\\\": true,//\" /etc/murano-conductor/data/templates/cf/Windows.template\n"
-expect "@murano-devbox"
+expect "#"
 
 send -- "service murano-api restart\n"
-expect "@murano-devbox"
+expect "#"
 send -- "service murano-conductor restart\n"
-expect "@murano-devbox"
+expect "#"
 
 send -- "exit\n"
