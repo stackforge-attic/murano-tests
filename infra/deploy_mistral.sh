882eaaa6bae77535734ad43b30d40e4dbdedb625
Sergey Murashov <smurashov@mirantis.com>
2014-01-13 13:04:27 +0400

    Change mistral launch method
    Change-Id: Ia2d288e426c922ca12e9a43d44e46ae58ca5b80d

--- infra/deploy_mistral.sh
+++ infra/deploy_mistral.sh
@@ -60,12 +60,5 @@ expect "@mistral"
 
 send -- "cd mistral\n"
 expect "@mistral"
-send -- "tox -evenv -- python mistral/cmd/api.py --config-file etc/mistral.conf &\n"
+send -- "su -m root -c 'tox -evenv -- python mistral/cmd/api.py --config-file etc/mistral.conf'\n"
 sleep 120
-send -- "\n"
-send -- "\n"
-send -- "\n"
-send -- "\n"
-send -- "\n"
-expect "@mistral"
-send -- "exit\n"
