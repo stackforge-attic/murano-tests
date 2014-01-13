74ff6cd64cb8f1001f1518de70c44fcd87c96091
Sergey Murashov <smurashov@mirantis.com>
2014-01-13 10:15:24 +0400

    Add script which deploy mistral
    Change-Id: Iff4266185687c10960939a34f516090bcd3b1ac4

---
+++ infra/deploy_mistral.sh
@@ -0,0 +1,59 @@
+set timeout 30
+
+send_user "\n\nStart to login to the test bed...\n\n"
+
+spawn /usr/bin/ssh  [lindex $argv 0]@[lindex $argv 1]
+expect "@mistral"
+
+send -- "sudo su\n"
+expect "@mistral"
+
+send -- "git clone https://github.com/stackforge/mistral -b "
+send -- [lindex $argv 3]
+send -- "\n"
+expect "@mistral"
+
+send -- "cp mistral/etc/mistral.conf.example mistral/etc/mistral.conf\n"
+expect "@mistral"
+
+send -- "sed -i \"s/rabbit_host = localhost/rabbit_host = "
+send -- [lindex $argv 2]
+send -- "/\" mistral/etc/mistral.conf\n"
+expect "@mistral"
+
+send -- "sed -i \"s/rabbit_port = 5672/rabbit_port = "
+send -- [lindex $argv 4]
+send -- "/\" mistral/etc/mistral.conf\n"
+expect "@mistral"
+
+send -- "sed -i \"s/rabbit_virtual_host = \\//rabbit_virtual_host = "
+send -- [lindex $argv 5]
+send -- "/\" mistral/etc/mistral.conf\n"
+expect "@mistral"
+
+send -- "sed -i \"s/rabbit_user = guest/rabbit_user = "
+send -- [lindex $argv 5]
+send -- "/\" mistral/etc/mistral.conf\n"
+expect "@mistral"
+
+send -- "sed -i \"s/rabbit_password = guest/rabbit_password = swordfish/\" mistral/etc/mistral.conf\n"
+expect "@mistral"
+
+send -- "sed -i \"s/auth_uri=http:\\/\\/localhost:5000\\/v3/auth_uri=http:\\/\\/"
+send -- [lindex $argv 2]
+send -- ":5000\\/v3/\" mistral/etc/mistral.conf\n"
+expect "@mistral"
+
+send -- "sed -i \"s/auth_host=localhost/auth_host="
+send -- [lindex $argv 2]
+send -- "/\" mistral/etc/mistral.conf\n"
+expect "@mistral"
+
+send -- "sed -i \"s/admin_user=admin/admin_user=AutotestUser/\" mistral/etc/mistral.conf\n"
+expect "@mistral"
+
+send -- "sed -i \"s/admin_password=password/admin_password=swordfish/\" mistral/etc/mistral.conf\n"
+expect "@mistral"
+
+send -- "sed -i \"s/admin_tenant_name=admin/admin_tenant_name=AutotestProject/\" mistral/etc/mistral.conf\n"
+expect "@mistral"
