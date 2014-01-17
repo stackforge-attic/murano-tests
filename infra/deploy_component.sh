e4f787921faa4b21d4408bfea56746c218ed4e51
Sergey Murashov <smurashov@mirantis.com>
2014-01-17 18:03:45 +0400

    Add fix in deploy component script
    Change-Id: I8b4c9a64b40292642e485e617a8b84ded2b7d1da

--- infra/deploy_component.sh
+++ infra/deploy_component.sh
@@ -28,9 +28,6 @@ send -- [lindex $argv 3]
 send -- "\n"
 expect "@murano"
 
-send -- "rm -rf setup.sh ; cp setupV2.sh setup.sh \n"
-expect "@murano"
-
 send -- "sh setup-centos.sh uninstall > 1.log\n"
 expect "@murano"
 send -- "sh setup.sh uninstall > 2.log\n"
@@ -40,6 +37,8 @@ send -- [lindex $argv 3]
 send -- " "
 send -- [lindex $argv 2]
 send -- " && git checkout FETCH_HEAD\n"
+expect "@murano"
+send -- "rm -rf setup.sh ; cp setupV2.sh setup.sh \n"
 expect "@murano"
 send -- "sh setup-centos.sh install > 3.log\n"
 expect "@murano"
