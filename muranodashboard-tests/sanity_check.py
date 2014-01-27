bb968ba36d70780ba51b04efbb624a29ffccfb00
Sergey Murashov <smurashov@mirantis.com>
2014-01-27 12:24:13 +0400

    Add 'create demo service' dashboard test
    Change-Id: I8acdaa98b1270d18143f56769287d237ea091756

--- muranodashboard-tests/sanity_check.py
+++ muranodashboard-tests/sanity_check.py
@@ -21,14 +21,23 @@ class UISanityTests(UITestCase):
         self.create_environment('test_edit_env')
         self.driver.find_element_by_link_text('test_edit_env')
         self.edit_environment(new_name='edited_env')
-        self.assertTrue(self.check_element_on_page(by.By.LINK_TEXT, 'edited_env'))
+        self.assertTrue(self.check_element_on_page(by.By.LINK_TEXT,
+                                                   'edited_env'))
 
     def test_rename_windows_image(self):
         self.log_in()
         self.navigate_to_images()
-        self.driver.find_element_by_id('marked_images__action_mark_image').click()
+        self.driver.find_element_by_id(
+            'marked_images__action_mark_image').click()
         self.select_from_list('image', 'ws-2012-std')
-        self.find_clean_send(by.By.ID, 'id_title', 'Windows Server 2012 Standard')
+        self.find_clean_send(by.By.ID, 'id_title',
+                             'Windows Server 2012 Standard')
         self.select_from_list('type', ' Windows Server 2012')
         mark = UITestCase.elements.get('button', 'ButtonSubmit')
         self.driver.find_element_by_xpath(mark).click()
+
+    def test_create_demo_service(self):
+        self.log_in()
+        self.navigate_to_environments()
+        self.create_environment('test')
+        self.create_demo_service('Demo Service')
