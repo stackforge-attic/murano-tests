52db078007273e7b6d0bf2e18144814f85564c6e
Sergey Murashov <smurashov@mirantis.com>
2014-02-11 13:24:51 +0400

    Add editing some parameters for murano-conductor.conf
    Change-Id: I56274fc6311ecd5b2fbce6d2314873f36d1606b9

--- infra/deploy_vm_new.sh
+++ infra/deploy_vm_new.sh
@@ -71,6 +71,11 @@ send -- [lindex $argv 5]
 send -- "/\" /etc/murano/conductor.conf\n"
 expect "@murano"
 
+send -- "sed -i \"s/ssl = False/ssl = "
+send -- [lindex $argv 5]
+send -- "/\" /etc/murano/murano-conductor.conf\n"
+expect "@murano"
+
 send -- "sed -i \"s/\\\"BootFromVolume\\\": true,//\" /etc/murano-conductor/data/templates/cf/Linux.template\n"
 expect "@murano"
 send -- "sed -i \"s/\\\"BootFromVolume\\\": true,//\" /etc/murano-conductor/data/templates/cf/Windows.template\n"
@@ -79,6 +84,11 @@ expect "@murano"
 send -- "sed -i \"s/murano_metadata_url = http:\\/\\/localhost:8084\\/v1/murano_metadata_url = http:\\/\\/"
 send -- [lindex $argv 1]
 send -- ":8084\\/v1/\" /etc/murano/conductor.conf\n"
+expect "@murano"
+
+send -- "sed -i \"s/murano_metadata_url = http:\\/\\/localhost:8084\\/v1/murano_metadata_url = http:\\/\\/"
+send -- [lindex $argv 1]
+send -- ":8084\\/v1/\" /etc/murano/murano-conductor.conf\n"
 expect "@murano"
 
 send -- "echo \"LANGUAGE_CODE='en'\" >> /etc/openstack-dashboard/local_settings.py\n"
