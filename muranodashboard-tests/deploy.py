026dca85bb2644157aa9d68c9c98fc8f26257444
Anastasia Kuznetsova <akuznetsova@mirantis.com>
2014-03-20 13:08:44 +0400

    Add a few new tests for WebUI
    - added tests for Tomcat and PostreSQL services
    - added tests for upload/delete agent template, ui file
      (and download appropriate file)
    - made some refactor
    
    Change-Id: I92967abc061d609adb6aab1e1b93e66bcd43795b

--- muranodashboard-tests/deploy.py
+++ muranodashboard-tests/deploy.py
@@ -9,7 +9,7 @@ import selenium.webdriver.common.by as by
 
 class UIDeployTests(UITestCase):
 
-    def test_035_deploy_demo_service(self):
+    def test_001_deploy_demo_service(self):
         self.log_in()
         self.navigate_to('Environments')
         self.create_environment('deploy_demo')
@@ -23,7 +23,7 @@ class UIDeployTests(UITestCase):
         self.check_the_status_of_env('deploy_demo', 'Ready')
         self.check_that_deploy_finished('deploy_demo')
 
-    def test_036_deploy_telnet_service(self):
+    def test_002_deploy_telnet_service(self):
         self.log_in()
         self.navigate_to('Environments')
         self.create_environment('deploy_telnet')
@@ -38,7 +38,7 @@ class UIDeployTests(UITestCase):
         self.check_the_status_of_env('deploy_telnet', 'Ready')
         self.check_that_deploy_finished('deploy_telnet')
 
-    def test_037_deploy_apache_service(self):
+    def test_003_deploy_apache_service(self):
         self.log_in()
         self.navigate_to('Environments')
         self.create_environment('deploy_apache')
@@ -53,7 +53,7 @@ class UIDeployTests(UITestCase):
         self.check_the_status_of_env('deploy_apache', 'Ready')
         self.check_that_deploy_finished('deploy_apache')
 
-    def test_038_deploy_ad_service(self):
+    def test_004_deploy_ad_service(self):
         self.log_in()
         self.navigate_to('Environments')
         self.create_environment('deploy_ad')
@@ -68,7 +68,7 @@ class UIDeployTests(UITestCase):
         self.check_the_status_of_env('deploy_ad', 'Ready')
         self.check_that_deploy_finished('deploy_ad')
 
-    def test_039_deploy_iis_service(self):
+    def test_005_deploy_iis_service(self):
         self.log_in()
         self.navigate_to('Environments')
         self.create_environment('deploy_iis')
@@ -83,7 +83,7 @@ class UIDeployTests(UITestCase):
         self.check_the_status_of_env('deploy_iis', 'Ready')
         self.check_that_deploy_finished('deploy_iis')
 
-    def test_040_deploy_asp_service(self):
+    def test_006_deploy_asp_service(self):
         self.log_in()
         self.navigate_to('Environments')
         self.create_environment('deploy_asp')
@@ -99,7 +99,7 @@ class UIDeployTests(UITestCase):
         self.check_that_deploy_finished('deploy_asp')
 
     @testtools.skip("There is no Neutron LB on lab")
-    def test_041_deploy_iis_farm_service(self):
+    def test_007_deploy_iis_farm_service(self):
         self.log_in()
         self.navigate_to('Environments')
         self.create_environment('deploy_iisfarm')
@@ -115,7 +115,7 @@ class UIDeployTests(UITestCase):
         self.check_that_deploy_finished('deploy_iisfarm')
 
     @testtools.skip("There is no Neutron LB on lab")
-    def test_042_deploy_asp_farm_service(self):
+    def test_008_deploy_asp_farm_service(self):
         self.log_in()
         self.navigate_to('Environments')
         self.create_environment('deploy_aspfarm')
@@ -130,7 +130,7 @@ class UIDeployTests(UITestCase):
         self.check_the_status_of_env('deploy_aspfarm', 'Ready')
         self.check_that_deploy_finished('deploy_aspfarm')
 
-    def test_043_deploy_mssql_service(self):
+    def test_009_deploy_mssql_service(self):
         self.log_in()
         self.navigate_to('Environments')
         self.create_environment('deploy_mssql')
@@ -145,7 +145,8 @@ class UIDeployTests(UITestCase):
         self.check_the_status_of_env('deploy_mssql', 'Ready')
         self.check_that_deploy_finished('deploy_mssql')
 
-    def test_044_deploy_sql_cluster_service(self):
+    @testtools.skip("https://bugs.launchpad.net/murano/+bug/1282097")
+    def test_010_deploy_sql_cluster_service(self):
         self.log_in()
         self.navigate_to('Environments')
         self.create_environment('deploy_mssql_cluster')
@@ -164,3 +165,97 @@ class UIDeployTests(UITestCase):
         self.navigate_to('Environments')
         self.check_the_status_of_env('deploy_mssql_cluster', 'Ready')
         self.check_that_deploy_finished('deploy_mssql_cluster')
+
+    def test_011_deploy_postgreSQL_service(self):
+        self.log_in()
+        self.navigate_to('Environments')
+        self.create_environment('deploy_postgreSQL')
+        self.env_to_service('deploy_postgreSQL')
+
+        self.create_postgreSQL_service('postgreSQL-serv')
+        self.assertTrue(self.check_element_on_page(by.By.LINK_TEXT,
+                                                   'postgreSQL-serv'))
+
+        self.driver.find_element_by_id('services__action_deploy_env').click()
+        self.navigate_to('Environments')
+        self.check_the_status_of_env('deploy_postgreSQL', 'Ready')
+        self.check_that_deploy_finished('deploy_postgreSQL')
+
+    def test_012_deploy_tomcat_service(self):
+        self.log_in()
+        self.navigate_to('Environments')
+        self.create_environment('deploy_tomcat')
+        self.env_to_service('deploy_tomcat')
+
+        self.create_postgreSQL_service('postgreSQL')
+        self.assertTrue(self.check_element_on_page(by.By.LINK_TEXT,
+                                                   'postgreSQL'))
+
+        self.driver.find_element_by_link_text('Create Service').click()
+        self.create_tomcat_service('tomcat', 'postgreSQL')
+        self.assertTrue(self.check_element_on_page(by.By.LINK_TEXT,
+                                                   'tomcat'))
+
+        self.driver.find_element_by_id('services__action_deploy_env').click()
+        self.navigate_to('Environments')
+        self.check_the_status_of_env('deploy_tomcat', 'Ready')
+        self.check_that_deploy_finished('deploy_tomcat')
+
+    def test_013_add_service_in_deployed_env(self):
+        self.log_in()
+        self.navigate_to('Environments')
+        self.create_environment('add_service_in_deployed_env')
+        self.env_to_service('add_service_in_deployed_env')
+
+        self.create_linux_telnet('telnet')
+        self.assertTrue(self.check_element_on_page(by.By.LINK_TEXT,
+                                                   'telnet'))
+
+        self.driver.find_element_by_id('services__action_deploy_env').click()
+        self.navigate_to('Environments')
+        self.check_the_status_of_env('add_service_in_deployed_env', 'Ready')
+        self.check_that_deploy_finished('add_service_in_deployed_env')
+
+        self.navigate_to('Environments')
+        self.env_to_service('add_service_in_deployed_env')
+        self.create_iis_service('iis')
+        self.assertTrue(self.check_element_on_page(by.By.LINK_TEXT, 'iis'))
+
+    def test_014_deploy_linux_windows_services(self):
+        self.log_in()
+        self.navigate_to('Environments')
+        self.create_environment('linux_windows_services')
+        self.env_to_service('linux_windows_services')
+
+        self.create_linux_telnet('telnet')
+        self.assertTrue(self.check_element_on_page(by.By.LINK_TEXT,
+                                                   'telnet'))
+
+        self.create_iis_service('iis')
+        self.assertTrue(self.check_element_on_page(by.By.LINK_TEXT, 'iis'))
+
+        self.driver.find_element_by_id('services__action_deploy_env').click()
+        self.navigate_to('Environments')
+        self.check_the_status_of_env('linux_windows_services', 'Ready')
+        self.check_that_deploy_finished('linux_windows_services')
+
+    def test_015_checking_type_and_last_operation(self):
+        self.log_in()
+        self.navigate_to('Environments')
+        self.create_environment('test')
+        self.env_to_service('test')
+
+        self.create_linux_telnet('telnet')
+        self.assertTrue(self.check_element_on_page(by.By.LINK_TEXT,
+                                                   'telnet'))
+        self.driver.find_element_by_id('services__action_deploy_env').click()
+
+        self.assertTrue(self.check_service_parameter(
+            'services', 'telnet', '3', 'Linux Telnet'))
+        self.assertTrue(self.check_service_parameter(
+            'services', 'telnet', '4', 'Deploy in progress'))
+        self.assertFalse(self.check_service_parameter(
+            'services', 'telnet', '5', 'Service draft created'))
+        self.navigate_to('Environments')
+        self.check_the_status_of_env('test', 'Ready')
+        self.check_that_deploy_finished('test')
